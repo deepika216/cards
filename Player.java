55382f6d00875bb8d86724d2a4105dd4071fb598
Chaitanya <Chaitanya.Arveti@cdk.com>
2016-07-07 23:23:25 +0530

    Constructor added to Player class

--- Player.java
+++ Player.java
@@ -1,5 +1,9 @@
 public class Player {
 	String playername;
 	int cardswithplayer;
+	public Player (String name, int numberofcards) {
+		this.playername = name;
+		this.cardswithplayer = numberofcards;
+	}
 	
 }
